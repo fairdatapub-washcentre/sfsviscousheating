4a5d993aa3a8141e703e7b702aa529170149b7fe
V-Po-V <leesannaidoo@gmail.com>
2024-11-28 14:24:50 +0000

    Import data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -12,6 +12,7 @@ library(openxlsx)
 
 # Read data --------------------------------------------------------------------
 # data_in <- readr::read_csv("data-raw/dataset.csv")
+data_in <- read_excel("data-raw/85% MC 2500 rpm.xlsx")
 # codebook <- readxl::read_excel("data-raw/codebook.xlsx") |>
 #  clean_names()
 
